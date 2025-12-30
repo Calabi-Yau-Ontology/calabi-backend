f6e955406e59ff2dab3edc036d876d85d4a95684
hwaxrang <hwaxang@gmail.com>
2025-12-30 17:38:33 +0900

    add origin CORS - #28

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { TransformInterceptor } from './common/interceptors/transform.intercepto
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  });
+
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(

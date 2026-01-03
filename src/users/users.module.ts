d31bf3d8f608902cb82e6e0e339b005473aaac5d
hou27 <ataj125@gmail.com>
2026-01-03 20:25:45 +0900

    refactor: drop user-settings table - #32

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,9 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { UserSettings } from './entities/user-settings.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, UserSettings])],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
